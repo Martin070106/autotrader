c313c28f4bd95a22bb981fa7029945a788be736a
Martin070106 <kameczmartin@gmail.com>
2025-10-17 11:56:06 +0200

    kesz

--- autotrader/Controllers/CarsController.cs
+++ autotrader/Controllers/CarsController.cs
@@ -23,5 +23,28 @@ namespace autotrader.Controllers
             }
            
         }
+        [HttpPost]
+        public ActionResult<Car> AddNewRecord(Car car)
+        {
+            using (var context = new CarDbContext())
+            {
+                var newCar = new Car
+                {
+                    Brand = car.Brand,
+                    Type = car.Type,
+                    Color = car.Color,
+                    Year = car.Year
+                };
+                if(newCar != null)
+                {
+                    context.Cars.Add(newCar);
+                    context.SaveChanges();
+                    return StatusCode(201, newCar);
+                }
+                return BadRequest(new { message = "Sikertelen feltöltés" });
+                
+            }
+
+        }
     }
 }
